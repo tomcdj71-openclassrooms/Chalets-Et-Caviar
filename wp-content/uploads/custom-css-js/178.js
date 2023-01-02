5f81a35efa3b64283d2acdc3e6793afeb50df86c
tomcdj71 <contact.tomc@yahoo.fr>
2023-01-02 15:45:13 +0100

    style: remove old css and js files

--- wp-content/uploads/custom-css-js/178.js
+++
@@ -1,5 +0,0 @@
-<!-- start Simple Custom CSS and JS -->
-<script type="text/javascript">
-for(var words=[["Address","Adresse"],["City","Ville"],["State","Département"],["Zip","Code Postal"],["Country","Pays"],["Open in Google Map","Ouvrir avec Google Maps"],["Main Detail","Caract\xe9ristiques principales"],["bedroom","Chambre"],["bathroom","Salle de bain"],["garage","Garage"],["Others Detail","Autres caract\xe9ristiques"],["Price","Prix"],["Gallery","Galerie"],],i=0;i<words.length;i++)for(var word=words[i],elements=document.querySelectorAll("body *"),j=0;j<elements.length;j++){var e,r=elements[j],a=r.innerHTML.replace(word[0],word[1]);r.innerHTML=a}
-</script>
-<!-- end Simple Custom CSS and JS -->
