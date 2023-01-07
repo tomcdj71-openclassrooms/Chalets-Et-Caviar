a4e182572f350f2f9994362d305fe34bfbffbec8
tomcdj71 <contact.tomc@yahoo.fr>
2023-01-07 11:01:36 +0100

    style: edit some new properties
    enh: translations updated
    
    enh: crop text when looking at the all properties view

---
+++ wp-content/uploads/custom-css-js/leftovers.js
@@ -0,0 +1,18 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+if (location.pathname.includes('nos-chalets')) {
+  const elements = document.querySelectorAll('p:not(:empty)');
+  const filteredElements = Array.from(elements).filter(element => element.textContent.includes('Contactez-nous'));
+  // if the page is /property-type/nos-chalets
+    filteredElements.forEach(element => {
+      element.innerHTML = element.innerHTML.replace(/Contactez.*/g, '');
+    });
+  //https://chalets-et-caviar.dew-it.dev/wp-admin/term.php?taxonomy=property-type&tag_ID=7&post_type=property
+  let EditPropertyTypesLink = document.querySelector('a[href*="taxonomy=property-type"]');
+  EditPropertyTypesLink.textContent = 'Editer Types de Bien';
+} else if (location.pathname.includes('property/')) {
+  let EditPropertyLink = document.querySelector('a[href*="action=edit"]'); // select the link
+  EditPropertyLink.textContent = 'Editer Ce Bien'; // change the text
+}
+</script>
+<!-- end Simple Custom CSS and JS -->
